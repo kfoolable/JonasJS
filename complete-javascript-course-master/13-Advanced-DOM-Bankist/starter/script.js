0d039591194831e16c60a800e2495b6e6d54901a
Kristine Gillian Gavino <50127731+kfoolable@users.noreply.github.com>
2023-11-29 03:31:21 +0800

    Added notes on section 13

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -8,8 +8,11 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
 const openModal = function (e) {
-  e.preventDefault(); // added to prevent webpage to jump to the top when the modal is opened
+  e.preventDefault(); // added to prevent webpage to jump to the top when the modal is opened which is the default behavior
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -33,8 +36,78 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords); // DOMRect
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset); // xoffset, from top to current element position
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  ); // height and width of current viewport
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // modern way
+  section1.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
+
+///////////////////////////////////////////////////////
+// Page navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault(); // prevents it to scroll to the id element
+
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({
+//       behavior: 'smooth',
+//     });
+//   });
+// });
+
+// More efficient way of implementing even listener to multiple elements -->
+
+// 1. Add even listener to common parent element
+// 2. Determine what element orignated event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  // console.log(e.target);
+  e.preventDefault();
+
+  // Matching strategy
+  // ignores clicks on elements that are not needed
+  if (e.target.classList.contains('nav__link')) {
+    // console.log('LINK');
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+});
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/*
 console.log(document.documentElement); // special way of selecting the entire document
 console.log(document.head);
 console.log(document.body);
@@ -73,3 +146,138 @@ document
     message.remove(); // very recent addition to JS
     // message.parentElement.removeChild(message); // old way
   });
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height); // nothing
+console.log(message.style.backgroundColor); // logs the background color we manually set in js
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height); // browser's calculated height // string result
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px'; // this doesn't work for me for some reason
+
+document.documentElement.style.setProperty('--color-primary', 'orangered'); // changes html root element color property, this is how to access root properties in js
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt); // Bankist logo
+console.log(logo.src); // http://127.0.0.1:8080/img/logo.png // absolute url
+console.log(logo.className); // has to be className and not class
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.designer); // undefined
+console.log(logo.getAttribute('designer')); // Jonas
+logo.setAttribute('company', 'Bankist'); // new attribute created
+
+console.log(logo.src); // absolute url
+console.log(logo.getAttribute('src')); // img/logo.png // relative url
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href); // http://127.0.0.1:8080/#
+console.log(link.getAttribute('href')); // #
+
+// Data attributes
+console.log(logo.dataset.versionNumber); // 3.0
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c', 'j');
+logo.classList.contains('c'); // returns boolean
+
+// Don't use
+logo.className = 'jonas';
+*/
+
+/*
+const h1 = document.querySelector('h1');
+
+// h1.addEventListener('mouseenter', function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// });
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+
+  // h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// Old-school way
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// };
+*/
+
+/*
+// Demonstrating bubbling events
+// rgb(255, 255, 255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+// console.log(randomColor(0, 255));
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor(); // this points to the document selected (.nav__link)
+  console.log('Link', e.target, e.currentTarget);
+  console.log(e.currentTarget === this); // true
+
+  // Stop propagation
+  // e.stopPropagation(); // stops propagation / travel to parent elements <-- generally not a good practice
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('Container', e.target);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('Nav', e.target);
+});
+*/
+
+/*
+// DOM Traversing
+// Walking through the DOM, meaning you can select an element based on another element
+
+const h1 = document.querySelector('h1');
+
+// Goind downwards: child
+console.log(h1.querySelectorAll('.highlight')); // only selects the child elements of h1
+console.log(h1.childNodes); // NodeList
+console.log(h1.children); // HTMLCollection
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+console.log(h1.parentNode); // logs header__title
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)'; // closest parent element, or closes header element was selected
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling); // null
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children); // gets the parent and the children elements
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
+*/
